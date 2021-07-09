e2174dafa520f6f79767665fb41d2b0d9c697669
Jacob Molsby <jacobmolsby@hotmail.se>
2021-07-09 21:26:50 +0000

    Add get and post function

---
+++ assets/js/script.js
@@ -0,0 +1,85 @@
+const API_KEY = "rlDy2lbO_JX0SUtiSs6lWLG76bU";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById('resultsModal'));
+
+// The e is passed in as event, its not used in this application.
+// But its good practice to pass in the evenet in eventlisternes.
+document.getElementById('status').addEventListener("click", e => getStatus(e));
+
+function displayErrors(data) {
+    
+    let heading = `JSHint results for ${data.file}`;
+
+    if (data.total_errors === 0) {
+        results = `<div class="no_errors">No errors reported</div>`;
+    } else {
+        results = `<div>Total Errors: <span class="error_count">${data.total_errors}</span></div>`;
+        for (let error of data.error_last) {
+            results += `<div>At line <span class="line">${error.line}</span>, `;
+            results += `column <span class="column">${error.col}</span></div>`;
+            results += `<div class="error">${error.error}</div>`;
+        }
+    }
+}
+
+function displayErrorsModal(data) {
+    let heading = 'Errors found';
+    let results = `<div>These are the errors found in your code: </div>`;
+    results += `<div class="error">${data.errors}</div>`;
+
+    document.getElementById('resultsModal').innerText = heading;
+    document.getElementById('results-content').innerHTml = results;
+
+    resultsModal.show();
+
+}
+
+async function getStatus(e) {
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        displayStatus(data);
+    } else {
+        throw new Error(data.error);
+    }
+}
+
+function displayStatus(data) {
+
+    let heading = "API Key Status";
+    let results = `<div>Your key is valid until</div>`;
+    results += `<div class="key-status">${data.expiry}</div>`;
+
+    document.getElementById('resultsModalTitle').innerText = heading;
+    document.getElementById('results-content').innerHTML = results
+
+    resultsModal.show()
+}
+
+
+document.getElementById('submit').addEventListener('click', e => postForm(e));
+
+async function postForm(e) {
+    const form = new FormData(document.getElementById("checksform"));
+
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Authorization": API_KEY,
+        },
+        body: form,
+    });
+
+    const data = await response.json();
+    
+    if (response.ok) {
+        displayErrors(data);
+    } else {
+        throw new Error(data.error);
+    }
+}
+
